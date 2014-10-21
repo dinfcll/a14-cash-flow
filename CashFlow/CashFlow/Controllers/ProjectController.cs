83ab290effff487d0c47e4d15d634f2ca480d0aa
DT420 <davtremblay@hotmail.com>
2014-10-21 13:54:09 -0400

    Fonction Rechercher
    Reste a faire les routes

--- CashFlow/CashFlow/Controllers/ProjectController.cs
+++ CashFlow/CashFlow/Controllers/ProjectController.cs
@@ -17,11 +17,13 @@ namespace CashFlow.Controllers
         NewProject.ProjectDBContext dbProjet = new NewProject.ProjectDBContext();
 
         public ActionResult Project()
-        {
+        {/*
             if (User.Identity.Name != "")
                 return View(new NewProject());
             else
                 return RedirectToAction("Login", "Account");
+          */
+            return View(new NewProject());
         }
 
         [HttpPost]
@@ -33,7 +35,7 @@ namespace CashFlow.Controllers
                 model.DateDepart = DateTime.Today;
                 model.MontantRequis = Convert.ToInt32(model.MontantString);
                 model.Createur = User.Identity.Name;
-
+                Recherche("fuck");
                 EnregistrerDansBD(model);
 
                 TempData["info"] = "Votre projet " + model.Titre + " est désormais lancé! Le financement prendra fin le "
@@ -86,6 +88,39 @@ namespace CashFlow.Controllers
             insert.ExecuteNonQuery();
             con.Close();
             
+        }
+
+        void Recherche(string MotCle)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=.\SQLEXPRESS;AttachDbFilename=|DataDirectory|\dbCashFlow.mdf;Integrated Security=True;User Instance=True");
+            con.Open();
+            List<string[]> Resultats = new List<string[]>();
+            try
+            {
+
+                SqlCommand Recherche = new SqlCommand("Select Hash,Titre,Description,Categorie FROM tableProject WHERE Titre LIKE '%" + MotCle + "%' OR Description LIKE '%" + MotCle + "%'", con);
+                SqlDataReader reader = Recherche.ExecuteReader();
+
+                int i = 0;
+                string[] projet = new string[4];
+                while (reader.Read())
+                {
+                    projet[0] = reader.GetValue(0).ToString();
+                    projet[1] = reader.GetValue(1).ToString();
+                    projet[2] = reader.GetValue(2).ToString();
+                    projet[3] = reader.GetValue(3).ToString();
+                    Resultats.Add(projet);
+                }
+
+            }
+            catch (Exception Ex)
+            {
+
+            }
+            finally
+            {
+                con.Close();
+            }
         }
 
     }
