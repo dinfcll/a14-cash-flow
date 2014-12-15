84d5d178680db9c9313fc861f32c4ebb2c587b1b
DT420 <davtremblay@hotmail.com>
2014-12-15 11:56:35 -0500

    dernier commit

--- CashFlow/CashFlow/Controllers/PayPalController.cs
+++ CashFlow/CashFlow/Controllers/PayPalController.cs
@@ -10,18 +10,33 @@ namespace CashFlow.Controllers
 {
     public class PayPalController : Controller
     {
-        public ActionResult ValidationCommande(string TitreProjet, string Montant)
+        public ActionResult ValidationCommande(string TitreProjet, string Montant, string Hash)
         {
             bool useSandBox = Convert.ToBoolean(ConfigurationManager.AppSettings["IsSandbox"]);
             var paypal = new PayPalModel(useSandBox);
 
             paypal.item_name = TitreProjet;
             paypal.amount = Montant;
+            Session["montant"] = Montant;
+            Session["hash"] = Hash;
             return View(paypal);
         }
 
         public ActionResult RedirectFromPaypal()
         {
+            SqlConnection con = new SqlConnection(@"Data Source=.\SQLEXPRESS;AttachDbFilename=|DataDirectory|\dbCashFlow.mdf;Integrated Security=True;User Instance=True");
+            con.Open();
+            SqlCommand read = new SqlCommand("Select MontantRecu FROM tableProject WHERE Hash = '" + Session["hash"].ToString() + "'", con);
+            SqlDataReader reader = read.ExecuteReader();
+            reader.Read();
+            int MontantRecu = reader.GetInt32(0);
+            reader.Close();
+
+            MontantRecu += Convert.ToInt32(Session["montant"]);
+            SqlCommand update = new SqlCommand("UPDATE tableProject SET MontantRecu = " + MontantRecu.ToString() + " WHERE  Hash = '" + Session["hash"] + "'", con);
+            update.ExecuteNonQuery();
+
+            con.Close();
             return View();
         }
 
